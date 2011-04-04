7874ab85ea8d84675069493f9b198b8ed79ca463
Andy Capiau <andy.capiau@geraardsbergen.be>
2011-04-04 14:47:31 +0100

    Deleting of "replacements" now works: I wrote something in the contract-model to handle deletes

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,20 +1,22 @@
 class Contract < ActiveRecord::Base
   belongs_to :function
   belongs_to :employee
-  has_many :replacements, :foreign_key => "replacer_id", :dependent => :destroy
-  has_many :replacing, :through => :replacements, :source => :replaced
-  has_many :reverse_relationships, :foreign_key => "replaced_id", :class_name => "Replacement", :dependent => :destroy
-  has_many :replacers, :through => :reverse_relationships, :source => :replacer
+  has_many :replacement_contracts, :foreign_key => "replacer_id", :class_name => "Replacement", :dependent => :destroy
+  has_many :replacing, :through => :replacement_contracts, :source => :replaced
+  has_many :replaced_contracts, :foreign_key => "replaced_id", :class_name => "Replacement", :dependent => :destroy
+  has_many :replacers, :through => :replaced_contracts, :source => :replacer
   has_many :evaluations
+
+  attr_accessor :should_destroy_replacement
 
   after_update :save_all_replacing
 
   def replaces!(replaced_contract)
-    replacements.create!(:replaced_id => replaced_contract.id)
+    self.replacement_contracts.create!(:replaced_id => extract_contract_id_from(replaced_contract))
   end
 
   def end_replacement!(replaced_contract)
-    replacements.find_by_replaced_id(replaced_contract).destroy
+    self.replacement_contracts.find_by_replaced_id(extract_contract_id_from(replaced_contract)).destroy
   end
 
   def description
@@ -24,21 +26,32 @@ class Contract < ActiveRecord::Base
   def replaced_contracts=(replaced_contracts)
     replaced_contracts.each do |replaced_contract|
       if replaced_contract[:replacement_id].blank?
-        self.replacements.build({:replacer_id => self.id, :replaced_id => replaced_contract[:id]})
+        replaces!(replaced_contract[:id].to_i)
       else
-        replacement = self.replacements.detect { |r| r.id == replaced_contract[:replacement_id].to_i }
+        replacement = self.replacement_contracts.detect { |r| r.id == replaced_contract[:replacement_id].to_i }
         replacement.replaced_id = replaced_contract[:id].to_i
+      end
+
+      unless replaced_contract[:should_destroy_replacement].blank?
+        Replacement.find(replaced_contract[:replacement_id]).destroy
       end
     end
   end
 
   def save_all_replacing
-    self.replacements.each do |r|
-      if r.should_destroy?
-        r.destroy
-      else
+    self.replacement_contracts.each do |r|
         r.save(false)
-      end
     end
+  end
+
+  private
+  
+  def extract_contract_id_from(replaced_contract)
+    if replaced_contract.class == Contract
+      replaced_contract_id = replaced_contract.id
+    elsif replaced_contract.is_a? Integer
+      replaced_contract_id = replaced_contract
+    end
+    replaced_contract_id
   end
 end
